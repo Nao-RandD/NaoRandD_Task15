73ef9e0116c0cc750b422b9f9b2db700e3b2f3dd
Nao-RandD <nao.r.and.d@gmail.com>
2021-07-06 08:09:47 +0900

    プロジェクトを追加

---
+++ AppDojoTask15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppDojoTask15
+//
+//  Created by Naoyuki Kan on 2021/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
